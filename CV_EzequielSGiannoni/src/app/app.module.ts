6e7c87db26da9bda436fd078a89909dd2c2592be
Ezequiel Giannoni <egiannoni@frba.utn.edu.ar>
2022-08-14 23:23:01 -0300

    Edit app.module.ts

--- CV_EzequielSGiannoni/src/app/app.module.ts
+++ CV_EzequielSGiannoni/src/app/app.module.ts
@@ -1,18 +1,49 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IniciarSesionComponent } from './componentes/iniciar-sesion/iniciar-sesion.component';
+import { PortfolioComponent } from './componentes/portfolio/portfolio.component';
+import { PieDePaginaComponent } from './componentes/pie-de-pagina/pie-de-pagina.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { LogoAPComponent } from './componentes/logo-ap/logo-ap.component';
+import { HttpClientModule } from '@angular/common/http';
+
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component' ;
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { ProyectoComponent } from './componentes/proyecto/proyecto.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { BannerComponent } from './componentes/banner/banner.component';
+import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
+import { HysComponent } from './componentes/hys/hys.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    IniciarSesionComponent
+    IniciarSesionComponent,
+    PortfolioComponent,
+    PieDePaginaComponent,
+    HeaderComponent,
+    FooterComponent,
+    BannerComponent,
+    ProyectoComponent,
+    EducacionComponent,
+    HysComponent,
+    LogoAPComponent,
+    AcercaDeComponent,
+    ExperienciaComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    NgCircleProgressModule.forRoot({
+      
+    }),
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
