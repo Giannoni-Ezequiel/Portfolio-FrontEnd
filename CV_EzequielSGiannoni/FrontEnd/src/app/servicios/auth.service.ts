6279d3238138d89e560df98310125899cc8611bb
Ezequiel Giannoni <egiannoni@frba.utn.edu.ar>
2022-08-19 01:17:28 -0300

    Add Servicios
    Agregando Auth y Token

---
+++ CV_EzequielSGiannoni/FrontEnd/src/app/servicios/auth.service.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Jwt } from '../model/jwt.dto';
+import { LoginUsuario } from '../model/login.usuario';
+import { NuevoUsuario } from '../model/nuevo-usuario';
+
+@Injectable
+({
+    providedIn: 'root'
+  })
+  export class AuthService 
+  {
+    
+    private url = environment.apiAuthUrl;;
+  
+  
+  
+    constructor(private httpClient: HttpClient) { }
+  
+    public nuevo(nuevoUsuario: NuevoUsuario): Observable<any>
+    {
+      return this.httpClient.post<any>(this.url + 'nuevo', nuevoUsuario);
+      
+    }
+  
+    public login(loginUsuario: LoginUsuario): Observable<Jwt>
+    {
+      return this.httpClient.post<Jwt>(this.url + 'login', loginUsuario);
+      
+    }
+  }
--- CV_EzequielSGiannoni/FrontEnd/src/app/servicios/persona.service.ts
+++ CV_EzequielSGiannoni/FrontEnd/src/app/servicios/persona.service.ts
@@ -1,17 +1,35 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/internal/Observable';
-import { persona } from '../model/nuevo-usuario';
+import { Persona } from '../model/persona.interface';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
-export class PersonaService {
-  URL = 'https://bkdprueba95.herokuapp.com/personas/';
+export class PersonaService 
+{
+  private apiServerUrl = environment.apiBaseUrl;
 
   constructor(private http: HttpClient) { }
-  
-  public getPersona(): Observable<persona>{
-    return this.http.get<persona>(this.URL+ 'traer/perfil');
+
+  public getPersonas(): Observable<Persona[]>
+  {
+    return this.http.get<Persona[]>(`${this.apiServerUrl}/persona/all`);
+  }
+
+  public addPersona(persona: Persona): Observable<Persona> 
+  {
+    return this.http.post<Persona>(`${this.apiServerUrl}/persona/add`, persona);
+  } 
+
+  public updatePersona(persona: Persona): Observable<Persona> 
+  {
+    return this.http.put<Persona>(`${this.apiServerUrl}/persona/update`, persona);
+  }
+
+  public deletePersona(personaId: number): Observable<void> 
+  {
+    return this.http.delete<void>(`${this.apiServerUrl}/persona/delete/${personaId}`);
   }
 }
