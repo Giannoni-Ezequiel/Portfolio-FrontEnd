bac28f63c702185b65b43e6d2078c6b4d1c7c109
Ezequiel Giannoni <egiannoni@frba.utn.edu.ar>
2022-08-22 17:22:26 -0300

    Reorganizando carpetas

--- CV_EzequielSGiannoni/FrontEnd/src/app/model/jwt.dto.ts
+++
@@ -1,7 +0,0 @@
-export interface Jwt
-{
-    token: string;
-    type: string;
-    nombreUsuario: string;
-    authorities: string[]; 
-}
--- CV_EzequielSGiannoni/FrontEnd/src/environments/environment.ts
+++
@@ -1,7 +0,0 @@
-export const environment = 
-{
-  production: true,
-  apiBaseUrl: 'http://localhost:8080',
-  apiAuthUrl: 'http://localhost:8080/auth/',
-};
-
