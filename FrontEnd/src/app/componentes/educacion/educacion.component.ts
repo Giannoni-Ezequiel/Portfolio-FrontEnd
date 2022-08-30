9231aa4b4243c6ff3a9e89b20de225eda1476c5f
Ezequiel <egiannoni@frba.utn.edu.ar>
2022-08-30 14:36:13 -0300

    EditDiseñoFront03

--- FrontEnd/src/app/componentes/educacion/educacion.component.ts
+++ FrontEnd/src/app/componentes/educacion/educacion.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Educacion } from 'src/app/model/educacion.interface';
+import { TokenService } from 'src/app/servicios/token.service';
+import { EducacionService } from 'src/app/servicios/educacion.service';
 
 @Component({
   selector: 'app-educacion',
@@ -6,10 +9,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./educacion.component.css']
 })
 export class EducacionComponent implements OnInit {
-
-  constructor() { }
-
+  educacion: Educacion[] = [];
+  constructor(private educacionS: EducacionService, private tokenService: TokenService) { }
+  isLogged = false;
   ngOnInit(): void {
+    this.cargarEducacion();
+    if(this.tokenService.getToken()){
+      this.isLogged = true;
+    } else {
+      this.isLogged = false;
+    }
   }
+  
+   cargarEducacion(): void{
+    this.educacionS.lista().subscribe(
+      data =>{
+        console.log(data);
+        this.educacion = data;
+      }
+    )
+   }
 
+   delete(id?: number){
+    if(id != undefined){
+      this.educacionS.delete(id).subscribe(
+        data => {
+          this.cargarEducacion();
+        }, err => {
+          alert("No se pudo eliminar");
+        }
+      )
+    }
+   }
 }
