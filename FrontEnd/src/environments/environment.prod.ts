59197d1bfc7482400abffbd87c5a2d8f8442e7b9
Ezequiel <egiannoni@frba.utn.edu.ar>
2022-08-31 16:22:01 -0300

    FrontFirebase

--- FrontEnd/src/environments/environment.prod.ts
+++ FrontEnd/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiBaseUrl: 'https://bkdprueba95.herokuapp.com/',
-  apiAuthUrl: 'https://bkdprueba95.herokuapp.com/auth/',
+  
 };
--- FrontEnd/src/environments/environment.ts
+++ FrontEnd/src/environments/environment.ts
@@ -2,6 +2,6 @@ export const environment =
 {
   production: true,
   apiBaseUrl: 'https://bkdprueba95.herokuapp.com/',
-  apiAuthUrl: 'https://bkdprueba95.herokuapp.com/auth/',
+  
 };
 
